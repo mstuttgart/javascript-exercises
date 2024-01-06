e2a53a20f31c08526184352bb0f9826a7133378d
Michell Stuttgart <michellstut@gmail.com>
2024-01-06 20:48:39 -0300

    Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
 
+  console.log(values)
+
+  for (let i = 0; i < values.length; i++) {
+    let idx = arr.indexOf(values[i]);
+
+    if (idx < 0) continue;
+
+    arr.splice(idx, 1);
+  }
+
+  return arr;
 };
 
 // Do not edit below this line
