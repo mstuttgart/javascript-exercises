315f8db904337a05c5fe4851eb65ce5e1fe421fc
Michell Stuttgart <michellstut@gmail.com>
2024-01-06 19:38:32 -0300

    Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+  if (count < 0) {
+    return "ERROR";
+  }
 
+  let finalWord = "";
+
+  for (let i = 0; i < count; i++) {
+    finalWord += word;
+  }
+
+  return finalWord;
 };
 
 // Do not edit below this line
