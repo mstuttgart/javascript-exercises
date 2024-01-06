dd6bd5b891c79553baa4629248b46d9c7ce851b7
Michell Stuttgart <michellstut@gmail.com>
2024-01-06 20:25:42 -0300

    Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let reverseStr = "";
 
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str.charAt(i);
+  }
+
+  return reverseStr;
 };
 
 // Do not edit below this line
