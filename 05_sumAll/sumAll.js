f7d0ef1f1f50d3e8a562654f5b058f5fa9ffeefe
Michell Stuttgart <michellstut@gmail.com>
2024-01-06 21:26:40 -0300

    Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return "ERROR";
+  }
+
+  let total = 0;
+
+  let start = Math.min(num1, num2);
+  let end = Math.max(num1, num2);
+
+  for (let i = start; i <= end; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
